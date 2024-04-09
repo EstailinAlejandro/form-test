22e433c3dc3d9182547403a2927461ca383ba00f
EstailinAlejandro <302159693@student.rocmondriaan.nl>
2024-04-09 15:24:02 +0200

    up

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -23,19 +23,60 @@ class FormController extends AbstractController
         ]);
     }
 
-    #[Route('/delete', name: 'delete')]
+    #[Route('/delete/{id}', name: 'delete')]
     public function delete(EntityManagerInterface $em, int $id): Response
     {
-        $employees = $em->getRepository(Employee::class)->find($id);
-        $em->remove($employees);
+        $employee = $em->getRepository(Employee::class)->find($id);
+
+        if (!$employee) {
+            // Optionally, handle the case where the entity does not exist
+            // You can redirect to an error page or display a flash message
+            // and redirect back to a listing page, for example.
+            // For simplicity, let's redirect back to the listing page
+            return $this->redirectToRoute('home');
+            $this->addFlash('warning', 'Er is iets mis gegaan');
+        }
+
+        $em->remove($employee);
+        $em->flush();
+        $this->addFlash('danger', 'Employee verwijderd');
+        // After successful deletion, you may want to redirect
+        // the user to another page or display a success message
+        // For simplicity, let's redirect back to the listing page
+        return $this->redirectToRoute('home');
+
         return $this->render('form/index.html.twig', [
             'employees' => $employees,
         ]);
     }
 
 
+    #[Route('/update/{id}', name: 'update')]
+    public function form(EmployeeRepository $repository, Request $request, EntityManagerInterface $em, int $id): Response
+    {
+        $employee = $em->getRepository(Employee::class)->find($id);
+        $form = $this->createForm(EmployeeType::class, $employee);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $employee = $form->getData();
+
+            $em->persist($employee);
+            $em->flush();
+
+            $this->addFlash('success', 'Employee aangepast');
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('form/form.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
     #[Route('/form', name: 'insert')]
-    public function form(EmployeeRepository $repository, Request $request, EntityManagerInterface $em): Response
+    public function update(EmployeeRepository $repository, Request $request, EntityManagerInterface $em): Response
     {
         $employee = new Employee();
         $form = $this->createForm(EmployeeType::class, $employee);
@@ -57,6 +98,4 @@ class FormController extends AbstractController
             'form' => $form,
         ]);
     }
-
-
 }
